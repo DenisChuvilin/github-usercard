dfb9c8fb9ba178bd2f141f315e5b08bef6ca16b7
DenisChuvilin <denischuv@gmail.com>
2020-02-13 16:18:24 -0800

    created elements

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,8 @@
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
+           https://api.github.com/users/DenisChuvilin
 */
-
+axios.get('https://api.github.com/users/DenisChuvilin').then(r => console.log(r));
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -45,7 +45,41 @@ const followersArray = [];
 </div>
 
 */
+function myFollowers(follower) {
+  const //create elements
+    card = document.createElement('div');
+    userImg = document.createElement('img');
+    cardInfo = document.createElement('div');
+    name = document.createElement('h3');
+    userName = document.createElement('p');
+    location = document.createElement('p');
+    profile = document.createElement('p');
+    link = document.createElement('a');
+    followers = document.createElement('p');
+    following = document.createElement('p');
+    bio = document.createElement('p');
+  // add classes
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  userName.classList.add('username');
+  // add src attributes
+  userImg.src = follower.data.avatar_url
+  link.src = follower.data.url
+  // add textcontent
+  name.textContent = follower.data.name
+  userName.textContent = follower.data.login
+  location.textContent = follower.data.location
+  link.textContent = follower.data.url
+  followers.textContent = follower.data.followers
+  following.textContent = follower.data.following
+  bio.textContent = follower.data.bio
+  // append
+card.append(userImg, cardInfo);
+cardInfo.append(name, userName, location, profile, followers, following, bio)
 
+
+}
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
