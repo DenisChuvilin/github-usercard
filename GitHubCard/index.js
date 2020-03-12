bb4095da4a067bbe17c271b7fad2cc202db8d122
DenisChuvilin <denischuv@gmail.com>
2020-03-12 07:52:56 -0700

    axios call and create elements done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,8 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/DenisChuvilin')
+.then(response => console.log(response.data))
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -45,7 +46,23 @@ const followersArray = [];
 </div>
 
 */
-
+// creating CardCreator function
+const CardCreator = (input) => {
+  //creating elements
+  const card = document.createElement('div');
+  const img = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const username = document.createElement('p');
+  const location = document.createElement('p');
+  const propfile = document.createElement('p');
+  const pagelink = document.createElement('a')
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+  
+return card;
+};
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
